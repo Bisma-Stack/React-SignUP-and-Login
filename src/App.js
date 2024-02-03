54ed8d6e8e61d5ca37888705b139ba5baea17f3b
Bisma <147504540+Biyadev@users.noreply.github.com>
2024-02-02 16:18:35 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Details from './Components/Details';
+import Errror from './Components/Errror';
+import {Routes,Route} from "router-dom"
+
+function App() {
+  return (
+  <>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/details' element={<Details />} />
+      <Route path='*' element={<Errror />} />
+    </Routes>
+  </>
+  );
+}
+
+export default App;
